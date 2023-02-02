2776b8fc2c8158281e4b388e535774004d42236b
Lucas Santos <lucassnts963@gmail.com>
2023-02-02 20:24:13 -0300

    feat: implementation of askAndReturnPrefix function

--- src/robots/input.ts
+++ src/robots/input.ts
@@ -2,15 +2,28 @@ import readline from "readline-sync";
 
 interface Content {
   searchTerm: string;
+  prefix: string;
 }
 
 export function robot() {
   const content = {} as Content;
 
   content.searchTerm = askAndReturnSearchTerm();
+  content.prefix = askAndReturnPrefix();
   console.log(content);
 
   function askAndReturnSearchTerm(): string {
     return readline.question("Type a search term: ");
+  }
+
+  function askAndReturnPrefix(): string {
+    const prefixes = ["What is", "Who is", "The history of"];
+    const selectedPrefixIndex = readline.keyInSelect(
+      prefixes,
+      "Choose one Option: "
+    );
+    const selectedPrefixText = prefixes[selectedPrefixIndex];
+
+    return selectedPrefixText;
   }
 }
